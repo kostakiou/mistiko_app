b4daf28c740998450f29e324b745bd1bdaf5892d
Giannis Dem <giannidis50@hotmail.com>
2015-05-04 12:11:23 +0300

    Added 1000 chars

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,4 +1,4 @@
 class Secret < ActiveRecord::Base
 	belongs_to :sxoleio
-	validates :title, presence: true, length: { maximum: 80 }
+	validates :title, presence: true, length: { maximum: 1000 }
 end
