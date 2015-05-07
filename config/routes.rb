8625439f5568499a68b51719d2519cd6cdd34452
Giannis Dem <giannidis50@hotmail.com>
2015-05-07 16:54:52 +0300

    delete and edit only for signd_in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users, :skip => [:registrations] 
-	as :user do
-  	get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-  	put 'users' => 'devise/registrations#update', :as => 'user_registration'
-	end
+  devise_for :users
   resources :secrets
 
   resources :posts
