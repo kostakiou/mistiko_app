4864ec824a73eb75080591ea578de2c6b5e63fa4
Giannis Dem <giannidis50@hotmail.com>
2015-05-07 14:45:36 +0300

    deleted sign_up/in ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users
+  devise_for :users, :skip => [:registrations] 
+	as :user do
+  	get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  	put 'users' => 'devise/registrations#update', :as => 'user_registration'
+	end
   resources :secrets
   root 'secrets#new'
 end
